9fb0aea2badf20997f3280b90430cea23c31d3f2
kjkGustavo <contato@gustavolima.dev>
2022-11-20 20:39:50 -0300

    chore: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1120px',
+      },
+      padding: '1rem'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
